a9b966df6e88661d1de9df9de08d8ba5bf2fd8f7
yixu9 <xy_1895@outlook.com>
2025-08-01 11:01:58 -0700

    clean working tree

--- crypto_arbitrage_detector/utils/get_quote_pair.py
+++ crypto_arbitrage_detector/utils/get_quote_pair.py
@@ -1,22 +1,24 @@
+import math
+from crypto_arbitrage_detector.utils.enrich_gas_fee import enrich_responses_with_gas_fee
+from crypto_arbitrage_detector.configs.request_config import jupiter_quote_api
+from crypto_arbitrage_detector.utils.data_structures import TokenInfo, EdgePairs
+from typing import List, Dict
+import aiohttp
+import asyncio
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import asyncio
-import aiohttp
-from typing import List, Dict
-from dataclasses import dataclass, field
-from crypto_arbitrage_detector.utils.data_structures import TokenInfo, EdgePairs
-from crypto_arbitrage_detector.configs.request_config import jupiter_quote_api, scraper_config, solana_rpc_api
-from crypto_arbitrage_detector.utils.simulate_gas_fee import fetch_swap_transaction, simulate_gas_fee
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
-import math
 
 # Function to prepare single request from Jupiter quote API
+
 async def fetch_quote(
-        session: aiohttp.ClientSession, 
-        input_mint: str, 
-        output_mint: str, 
-        #semaphore: asyncio.Semaphore, 
+        session: aiohttp.ClientSession,
+        input_mint: str,
+        output_mint: str,
+        # semaphore: asyncio.Semaphore,
         amount: int = jupiter_quote_api["default_tx_amount"]) -> Dict:
     '''
     Fetch quote from Jupiter API for a given token pair.
@@ -31,35 +33,38 @@ async def fetch_quote(
     params = {
         "inputMint": input_mint,
         "outputMint": output_mint,
-        "amount": amount,
-        "slippageBps": jupiter_quote_api["default_slippage_bps"]
+        "amount": amount
+        # "slippageBps": jupiter_quote_api["default_slippage_bps"]
     }
     # Set headers to mimic a browser request
     headers = jupiter_quote_api["headers"]
-    #async with semaphore:
+    # async with semaphore:
     try:
         async with session.get(
-            jupiter_quote_api["base_url"], 
-            params=params, 
-            headers=headers, 
-            #proxy=strategy_config.PROXY_URL, 
-            #timeout=15
-            #ssl=False
-            ) as resp:
-            #print(f"[DEBUG] Request URL: {resp.url}")
+            jupiter_quote_api["base_url"],
+            params=params,
+            headers=headers,
+            # proxy=strategy_config.PROXY_URL,
+            # timeout=15
+            # ssl=False
+        ) as resp:
+            # print(f"[DEBUG] Request URL: {resp.url}")
             if resp.status == 200:
                 quote_data = await resp.json()
-                #print(f"[DEBUG] Response Data: {quote_data}")
+                # print(f"[DEBUG] Response Data: {quote_data}")
                 return quote_data
             else:
-                print(f"Non-200 response: {resp.status} | {input_mint} {output_mint}")
+                print(
+                    f"Non-200 response: {resp.status} | {input_mint} {output_mint}")
                 return {}
-        
+
     except Exception as e:
         print(f"Error fetching quote: {e}")
         return {}
 
 # Function to request data from Jupiter API for edge pairs
+
+
 async def get_edge_pairs(token_list: List[TokenInfo], tx_amount: int = jupiter_quote_api["default_tx_amount"]) -> List[EdgePairs]:
     '''
     Fetch edge pairs from Jupiter API for all token combinations.
@@ -68,7 +73,7 @@ async def get_edge_pairs(token_list: List[TokenInfo], tx_amount: int = jupiter_q
     Returns:
         List[EdgePairs]: List of EdgePairs objects containing quote data.
     '''
-    #semaphore = asyncio.Semaphore(5)
+    # semaphore = asyncio.Semaphore(5)
     edge_pairs = []
     # create all requests for each token pair
     async with aiohttp.ClientSession() as session:
@@ -77,16 +82,19 @@ async def get_edge_pairs(token_list: List[TokenInfo], tx_amount: int = jupiter_q
             for token_out in token_list:
                 if token_in.address != token_out.address:
                     tasks.append(fetch_quote(
-                        session, 
-                        token_in.address, 
+                        session,
+                        token_in.address,
                         token_out.address,
-                        #semaphore,
+                        # semaphore,
                         tx_amount
-                        ))
+                    ))
 
     # execute all requests concurrently
         responses = await asyncio.gather(*tasks)
-    
+        responses = [
+            r for r in responses if r and "inputMint" in r and "routePlan" in r]
+        responses = await enrich_responses_with_gas_fee(responses)
+
     # generate a price map in order to calculate the total_fee in SOL
     price_map = generate_price_map_from_responses(responses)
 
@@ -115,42 +123,33 @@ async def get_edge_pairs(token_list: List[TokenInfo], tx_amount: int = jupiter_q
                         fee = float(fee_str)
                         price_in_sol = price_map.get(fee_mint, 0.0)
                         total_fee_sol += fee * price_in_sol
-              
+
                 # Handle platform fee if return null
-                platform_fee_info = data.get("platformFee"),
+                platform_fee_info = data.get("platformFee")
                 if isinstance(platform_fee_info, dict):
                     platform_fee = float(platform_fee_info.get("amount", 0))
                 else:
                     platform_fee = 0.0
-                
-                try:
-                    swap_tx = fetch_swap_transaction(data)
-                    gas_fee = simulate_gas_fee(swap_tx)
-                except Exception as e:
-                    # use base fee in Solana RPC API configuration
-                    print(f"Error in gas fee simulation: {e}")
-                    gas_fee = solana_rpc_api["base_fee"]
 
-                # tx = jupiter_quote_api.get("fetch_swap_transaction", False)
-                # if tx:
-                #     # Simulate gas fee if enabled
-                #     gas_fee = await simulate_gas_fee(tx)
-                # else:
-                #     gas_fee = solana_rpc_api["base_fee"]
+                out_mint = data["outputMint"]
+                out_amount_in_sol = out_amount * price_map.get(out_mint, 0.0)
 
+                in_mint = data["inputMint"]
+                in_amount_in_sol = in_amount * price_map.get(in_mint, 0.0)
 
                 # Create EdgePairs object
                 edge = EdgePairs(
                     from_token=data["inputMint"],
                     to_token=data["outputMint"],
-                    out_amount=out_amount,
+                    in_amount=in_amount_in_sol,
+                    out_amount=out_amount_in_sol,
                     price_ratio=price_ratio,
                     weight=weight,
                     slippage_bps=data.get("slippageBps", 0),
                     platform_fee=platform_fee if platform_fee is not None else 0.0,
                     price_impact_pct=float(data.get("priceImpactPct", 0.0)),
                     total_fee=total_fee_sol,
-                    gas_fee=gas_fee  # gas fee in lamports
+                    gas_fee=data["gasFee"]  # gas fee in lamports
                 )
                 edge_pairs.append(edge)
             except Exception as e:
@@ -180,7 +179,8 @@ def generate_price_map_from_responses(responses: List[Dict]) -> Dict[str, float]
 
             if in_mint == sol and out_amt > 0:
                 # 1 SOL = ? other_token
-                price = 1 / (out_amt / in_amt)  # other_token/SOL → SOL/other_token
+                # other_token/SOL → SOL/other_token
+                price = 1 / (out_amt / in_amt)
                 price_map[out_mint] = price
             elif out_mint == sol and in_amt > 0:
 
